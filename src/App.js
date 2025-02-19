031f42758ad86dc5385c507d183be3076cbf7607
Ufuk Cefaker <139013221+UfukCefaker@users.noreply.github.com>
2025-02-19 15:59:55 +0300

    Yeni quiz sayfaları ve tasarım eklendi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
+// App.js
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import Quiz from './components/Quiz';
 import ResultPage from './components/ResultPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/quiz/:questionNumber" element={<Quiz />} />
+      <Route path="/result" element={<ResultPage />} />
+    </Routes>
   );
 }
-// <Route path="/quiz/:questionNumber" element={<Quiz />} />
-// <Route path="/result" element={<ResultPage />} />
+
 export default App;
+
 
