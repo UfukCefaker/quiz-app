232623a9448559510debafb88ca0c39373e33f01
Ufuk Cefaker <139013221+UfukCefaker@users.noreply.github.com>
2025-02-19 15:21:46 +0300

    Updates

---
+++ precache-manifest.aec058bd18408efebaa03a104576d52a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7f7cbf8a4254eb8b0077ff445760cb",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "f501a53142289d163a2f",
+    "url": "/quiz-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/quiz-app/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/quiz-app/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "f501a53142289d163a2f",
+    "url": "/quiz-app/static/js/main.28213d90.chunk.js"
+  },
+  {
+    "revision": "54eef569632a95db8902",
+    "url": "/quiz-app/static/js/runtime~main.912b8294.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/quiz-app/static/media/logo.06e73328.svg"
+  }
+]);
